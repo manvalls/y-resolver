d0454a6f9d4de2f2772935bbe8c4dc8967358f83
Manuel Valls Fernández <manolo@vlrz.es>
2015-01-14 18:08:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Resolver
+
+## Sample usage
+
+```javascript
+var Resolver = require('y-resolver'),
+    res = new Resolver(),
+    yd = res.yielded();
+
+setTimeout(function(){
+  res.accept('hi');
+});
+
+if(!yd.done) yd.listen(function(){
+  if(this.accepted) console.log(this.value);
+  else throw this.error;
+});
+
+```
---
+++ main.js
@@ -0,0 +1,82 @@
+var Su = require('vz.rand').Su,
+    
+    listeners = Su(),
+    lArgs = Su(),
+    
+    accepted = Su(),
+    rejected = Su(),
+    done = Su(),
+    value = Su(),
+    error = Su(),
+    
+    Resolver;
+
+// Yielded
+
+function Yielded(){
+  this[listeners] = [];
+  this[lArgs] = [];
+  
+  this[done] = false;
+  this[rejected] = false;
+  this[accepted] = false;
+}
+
+Object.defineProperties(Yielded.prototype,{
+  
+  listen: {value: function(callback,args){
+    this[listeners].push(callback);
+    this[lArgs].push(args || []);
+  }},
+  
+  done: {get: function(){ return this[done]; }},
+  accepted: {get: function(){ return this[accepted]; }},
+  rejected: {get: function(){ return this[rejected]; }},
+  
+  error: {get: function(){ return this[error]; }},
+  value: {get: function(){ return this[value]; }}
+  
+});
+
+// Resolver
+
+module.exports = Resolver = function Resolver(){
+  Object.defineProperty(this,'yielded',{value: new Yielded()});
+};
+
+Object.defineProperties(Resolver.prototype,{
+  
+  reject: {value: function(e){
+    var i,cb,args;
+    
+    if(this.yielded.done) return;
+    
+    this.yielded[done] = true;
+    this.yielded[rejected] = true;
+    this.yielded[error] = e;
+    
+    while(cb = this.yielded[listeners].shift()){
+      args = this.yielded[lArgs].shift();
+      cb.apply(this.yielded,args);
+    }
+    
+  }},
+  
+  accept: {value: function(v){
+    var i,cb,args;
+    
+    if(this.yielded.done) return;
+    
+    this.yielded[done] = true;
+    this.yielded[accepted] = true;
+    this.yielded[value] = v;
+    
+    while(cb = this.yielded[listeners].shift()){
+      args = this.yielded[lArgs].shift();
+      cb.apply(this.yielded,args);
+    }
+    
+  }}
+  
+});
+
